7d38013dce2baa5e08d30b865498c60fe6d77c36
susan <2496127467@qq.com>
2017-07-17 21:10:59 +0800

    添加任务完成

--- js/app.js
+++ js/app.js
@@ -5,18 +5,43 @@
 
 	angular
 	.module('todoApp',[])
-	.controller('TodoController',['$scope',function($scope){
+	.controller('TodoController',['$scope', todoHandler]);
+	function todoHandler($scope){
 		//视图模型 vm --> viewmodel
 		var vm = $scope;
 
 		//1.展示任务列表
 		var todoList = [
-			{id:1,name:'jack',isCompleted:false},
-			{id:2,name:'rose',isCompleted:true},
-			{id:3,name:'tina',isCompleted:true},
-			{id:4,name:'susan',isCompleted:false},
+			{id:1,name:'jack',isCompleted:true},
+			{id:2,name:'tina',isCompleted:true},
+			{id:3,name:'susan',isCompleted:false}
 		]
 
 		vm.todoList = todoList;
-	}])
+
+		//添加任务(用户输入的任务)
+		vm.taskName = '';
+			console.log(vm.taskName);
+		vm.addTodo = function(){
+			console.log('dd')
+			if(vm.taskName.trim() === ''){
+				return;
+			}
+
+			var ids,
+				length = todoList.length;
+
+			//如果数组中没有值，则任务id为 1
+			//如果数组中有值，则任务id为 数组中最后一项id值 + 1
+			if(length === 0){
+				ids = 1;
+			}else{
+				ids = todoList[length - 1].id + 1;
+			}
+
+			//将创建的任务追加到数组中 并 清空输入框
+			todoList.push({id:ids,name:vm.taskName,isCompleted:false});
+			vm.taskName = '';
+		}
+	}
 })(window);
