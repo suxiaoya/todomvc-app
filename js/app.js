8cc07cff7ffe73d3e421f25e953d4e2865491c2f
susan <2496127467@qq.com>
2017-07-17 20:18:01 +0800

    展示任务列表完成

--- js/app.js
+++ js/app.js
@@ -3,4 +3,20 @@
 
 	// Your starting point. Enjoy the ride!
 
+	angular
+	.module('todoApp',[])
+	.controller('TodoController',['$scope',function($scope){
+		//视图模型 vm --> viewmodel
+		var vm = $scope;
+
+		//1.展示任务列表
+		var todoList = [
+			{id:1,name:'jack',isCompleted:false},
+			{id:2,name:'rose',isCompleted:true},
+			{id:3,name:'tina',isCompleted:true},
+			{id:4,name:'susan',isCompleted:false},
+		]
+
+		vm.todoList = todoList;
+	}])
 })(window);
